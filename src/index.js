bcb5117520e9ed60524a77d60e529b2a8c189248
Syava7 <syava77787@gmail.com>
2019-05-29 15:53:00 +0300

    structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-
-const getResource = async (url) => {
-  const res = await fetch(url);
-  const body = await res.json();
-  return body;
-};
-
-getResource('https://swapi.co/api/people/1/')
-  .then((body) => {
-    console.log(body);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-
+ReactDOM.render(<App />,
+  document.getElementById('root'));
