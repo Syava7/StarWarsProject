3f30534a6d93fa7875d0afa62f79be184075d302
Syava7 <syava77787@gmail.com>
2019-06-01 15:04:29 +0300

    life cycles

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -14,12 +14,12 @@ export default class RandomPlanet extends Component {
     loading: true
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 5000);
   }
 
-  onPlanetLoaded = (planet) => {
+  onPlanetLoaded = (planet) => { 
     this.setState({
       planet,
       loading: false,
@@ -34,8 +34,8 @@ export default class RandomPlanet extends Component {
     });
   };
 
-  updatePlanet() {
-    const id = Math.floor(Math.random()*25) + 2;
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
